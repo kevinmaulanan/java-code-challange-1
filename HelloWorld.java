d485a03b89f8996b3e62b8fd27c7469c99cafeb7
Kevin Maulana <kevinmaulanan@gmail.com>
2021-10-12 16:06:41 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String first_name = "Kevin";
+        String last_name = "Maulana Nasrullah";
+        int age = 20;
+        String phone_number = "081395760945";
+        String address = "Jl Pintung Ledeng Ciomas Bogor";
+        System.out.println("First Name: " + first_name);
+        System.out.println("Last Name: " + last_name);
+        System.out.println("Age: " + age);
+        System.out.println("Phone Number: " + phone_number);
+        System.out.println("Address: " + address);
+    }
+}
